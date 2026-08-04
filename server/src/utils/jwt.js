bf9f2463f116cda31f88ef8b063b48a6f709152d
musharraf10 <skmusharaf00@gmail.com>
2026-08-04 21:44:40 +0530

    feat: added cors both local host and netlify Url

--- server/src/utils/jwt.js
+++ server/src/utils/jwt.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-import { env } from "../config/env";
+import { env } from "../config/env.js";
 
 const TOKEN_COOKIE_NAME = "token";
 
 export const generateToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN || "7d",
+  return jwt.sign({ id: userId }, env.JWT_SECRET, {
+    expiresIn: env.JWT_EXPIRES_IN || "7d",
   });
 };
 
@@ -13,7 +13,7 @@ export const sendTokenCookie = (res, token) => {
   res.cookie(TOKEN_COOKIE_NAME, token, {
     httpOnly: true,
     secure: env.NODE_ENV === "production",
-    sameSite: "lax",
+    sameSite: env.NODE_ENV === "production" ? "none" : "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
